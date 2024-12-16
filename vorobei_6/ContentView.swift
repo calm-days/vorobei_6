9be53363ac5e713681de59b119484a16300a0502
Roman Liukevich <Raman.Liukevich@ringieraxelspringer.pl>
2024-12-16 16:52:21 +0100

    initial commit

--- vorobei_6/ContentView.swift
+++ vorobei_6/ContentView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var viewType: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height
+            let safeAreaTop = geometry.safeAreaInsets.top
+
+            let number = 4
+
+            ZStack {
+                ForEach(0..<number) { index in
+                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                        .fill(viewType ? Color.indigo : Color.teal)
+                        .frame(
+                            width: viewType ? screenHeight / CGFloat(number) : (screenWidth - CGFloat(number - 1) * 5) / CGFloat(number),
+                            height: viewType ? screenHeight / CGFloat(number) : (screenWidth - CGFloat(number - 1) * 5) / CGFloat(number)
+                        )
+                        .offset(
+                            x: viewType
+                            ? (screenWidth - (screenHeight / CGFloat(number))) - ((screenWidth / CGFloat(number + 1)) * CGFloat(index))
+                            : screenWidth - (((screenWidth - CGFloat(number - 1) * 5) / CGFloat(number)) * CGFloat(index + 1)) - (CGFloat(index) * 5),
+                            y: viewType
+                            ? (screenHeight / CGFloat(number)) * CGFloat(index) + safeAreaTop
+                            : screenHeight / 2
+                        )
+                        .animation(.snappy(duration: 0.65), value: viewType)
+                }
+            }
+            .ignoresSafeArea()
+            .onTapGesture {
+                viewType.toggle()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
